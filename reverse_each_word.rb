fc886f478ad712b4e8dc282ec6688b1f80d4be6c
Adit P Patel <app2116@gmail.com>
2020-01-14 21:43:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word (sentance)
   temp = sentance.split(' ')
-  result = temp.each{|n| n.reverse}
+  result = temp.each{|n| puts n.reverse}
   p result
   return result.join(' ')
 end
